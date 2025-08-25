9ddeab60bcaef152d1827100c5f6fbba76d946d4
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-08-25 14:11:23 -0300

    Criando o arquivo de rotas do projeto

---
+++ users/index.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const router = express.Router();
+const path = require('path');
+
+const basePath = path.join(__dirname, '../templates');
+
+router.get('/:id', (req, res) => {
+    const id = req.params.id;
+    console.log(`Estamos buscando pelo usuário: ${id}`);
+    res.sendFile(`${basePath}/users.html`);
+});
+
+module.exports = router;
